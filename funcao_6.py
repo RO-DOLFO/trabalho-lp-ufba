b9796877f5a4930ef0573e1dc35b447bbcce3e7a
RO-DOLFO <116039574+RO-DOLFO@users.noreply.github.com>
2025-06-17 17:01:30 -0300

    Add files via upload

--- funcao_6.py
+++ funcao_6.py
@@ -7,5 +7,35 @@ a primeira conterá apenas a letra ’f’;
 a segunda conterá uma lista de números inteiros. 
 
 #saida
-Essa operação gera, na saída, os números por ordem crescente de frequência de ocorrência. Os números que ocorrerem mais de uma vez devem aparecer uma única vez na saída. Após cada número deve aparecer um espaço, seguido da quantidade de vezes em que ocorre, entre parênteses. Por exemplo, suponha que o número 4 ocorra cinco vezes na lista e o número 10, uma vez. A saída deve ser: 10 (1) 4 (5). Os números com mesma frequência de ocorrência devem aparecer em ordem crescente.
+Essa operação gera, na saída, os números por ordem crescente de frequência de ocorrência.
+Os números que ocorrerem mais de uma vez devem aparecer uma única vez na saída.
+
+Após cada número deve aparecer um espaço, seguido da quantidade de vezes em que ocorre, entre parênteses. 
+
+Por exemplo, suponha que o número 4 ocorra cinco vezes na lista e o número 10, uma vez. A saída deve ser: 10 (1) 4 (5). Os números com mesma frequência de ocorrência devem aparecer em ordem crescente.
 '''
+
+def ordena_por_frequencia_de_ocorrencia(l1):
+    lista_sem_rep=[]
+    cont=0
+
+    for i in range(len(l1)):
+        if(l1[i] == l1[i]+1):
+            cont+=1
+
+# tira as repetições 
+    for item in l1:
+        if(item not in lista_sem_rep):
+            lista_sem_rep.append(item)
+
+# mostra a lista sem repetições
+    for i in lista_sem_rep:
+        print(i, end=' ')
+
+    print('')
+#lista full
+    for i in range(len(l1)):
+        print(l1[i], end=' ')
+
+
+#teste
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from funcoes.funcao_2 import pares_antes_de_impares_em_ordem_crescente
 from funcoes.funcao_3 import intercala_duas_listas
 from funcoes.funcao_4 import intercala_sem_repeticao
 from funcoes.funcao_5 import intercalar_tres_listas
+from funcoes.funcao_6 import ordena_por_frequencia_de_ocorrencia
+from funcoes.funcao_7 import pares_antes_de_impares_ordena_dentro_da_categoria_e_intercala
 
 var = input()
 
@@ -97,5 +99,38 @@ if(var == 'j'):
         lista3_de_numeros.append(numero)
 
     intercalar_tres_listas(lista1_de_numeros, lista2_de_numeros, lista3_de_numeros)
+
+#questão 6
+if(var == 'f'):
+    lista = input()
+    lista = lista.split()
+    lista_de_numeros = []
+
+    for item in lista:
+        numero = int(item)
+        lista_de_numeros.append(numero)
+
+    ordena_por_frequencia_de_ocorrencia(lista_de_numeros)
+
+#questão 7
+if(var == 'm'):
+    lista1 = input()
+    lista1 = lista1.split()
+    lista1_de_numeros = []
+
+    for item in lista1:
+        numero = int(item)
+        lista1_de_numeros.append(numero)
+
+    lista2 = input()
+    lista2 = lista2.split()
+    lista2_de_numeros = []
+
+    for item in lista2:
+        numero = int(item)
+        lista2_de_numeros.append(numero)
+
+    pares_antes_de_impares_ordena_dentro_da_categoria_e_intercala(lista1_de_numeros, lista2_de_numeros)
+
 
 
